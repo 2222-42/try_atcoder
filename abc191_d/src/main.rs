be180695b026c11b561f06d7dc31199f753c0067
2222-42 <stephen0617@me.com>
2021-02-17 07:31:10 +0900

    fix(abc191_d): marginal case

--- abc191_d/src/main.rs
+++ abc191_d/src/main.rs
@@ -9,29 +9,40 @@ fn main() {
 
     let mut count = 0;
 
+    let coeff: i64 = 10000;
+
     let (x, y, r) = (
-        (x * 10000.0) as i64,
-        (y * 10000.0) as i64,
-        (r * 10000.0) as i64,
+        (x * coeff as f64).round() as i64,
+        (y * coeff as f64).round() as i64,
+        (r * coeff as f64).round() as i64,
     );
 
-    let max_x = x + r;
-    let min_x = x - r;
-    let range_of_x: Vec<i64> = (min_x..=max_x)
-        .map(i64::from)
-        .filter(|&x| x % 10000 == 0)
-        .collect();
+    let min_x = (x - r + coeff - 1).div_euclid(coeff) * coeff;
+    let max_x = (x + r).div_euclid(coeff) * coeff;
 
-    for target_x in range_of_x {
+    let mut target_x = min_x;
+    while target_x <= max_x {
         let div_x = target_x - x;
-        let div_y = ((r * r - div_x * div_x) as f64).sqrt();
-        let min_y = (y as f64) - div_y;
-        let max_y = (y as f64) + div_y;
-        let range_of_y: Vec<i64> = (min_y.ceil() as i64..=(max_y.ceil() as i64))
-            .map(i64::from)
-            .collect();
-        count += range_of_y.iter().filter(|&&y| y % 10000 == 0).count()
+        let div_y = newton_sqrt(r * r - div_x * div_x);
+        let min_y = (y - div_y + coeff - 1).div_euclid(coeff);
+        let max_y = (y + div_y).div_euclid(coeff);
+        count += (max_y - min_y) + 1;
+
+        target_x += coeff;
     }
 
     println!("{}", count);
+}
+
+fn newton_sqrt(n: i64) -> i64 {
+    let mut x0 = n / 2;
+    if x0 == 0 {
+        return n;
+    }
+    let mut x1 = (x0 + n / x0) / 2;
+    while (x1 < x0) {
+        x0 = x1;
+        x1 = (x0 + n / x0) / 2;
+    }
+    x0
 }
